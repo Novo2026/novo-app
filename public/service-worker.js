c76e240d4449755155a6c1ccb93e5e9a633f3210
Novo2026 <ben@windmillmortgage.com>
2026-01-30 16:56:05 -0500

    Updated index.html

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,5 @@
+const CACHE_NAME = 'novo-cache-v1';
+
 self.addEventListener('install', (event) => {
   console.log('NOVO Service Worker installed');
   self.skipWaiting();
@@ -5,9 +7,42 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('activate', (event) => {
   console.log('NOVO Service Worker activated');
-  return self.clients.claim();
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log('Clearing old cache:', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => {
+      console.log('Taking control of all clients');
+      return self.clients.claim();
+    })
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    console.log('SKIP_WAITING message received, activating new service worker');
+    self.skipWaiting();
+  }
 });
